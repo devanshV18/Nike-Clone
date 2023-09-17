8061a088ba4ad4151bef6a425b5d8377d5806915
devanshV18 <devanshverma024@gmail.com>
2023-09-17 20:10:45 +0530

    problems with hero

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,8 +1,21 @@
+import Button from '../Components/Button';
+import { arrowRight } from '../assets/icons';
 const Hero = () => {
   return (
-    <div>
-      
-    </div>
+   <section id="home" className="w-full flex xl:flex-row flex-col justify-center min-h-screengap-10 max-container">
+      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
+          <p>Our Summer Collection</p>
+            <h1>
+              <span>The New Arrival</span>
+              <br />
+              <span>Nike</span>
+              Shoes
+            </h1>
+            <p>Discover Stylish Nike arrivals, quality comfort, and innovation for your active life</p>
+            {/* <Button label = "Shop Now"  
+            iconURL={arrowRight}/> */}
+      </div>
+   </section>
   )
 }
 
