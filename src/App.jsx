0904b406a9e5f5960efaaf6fdeaffb1a9fb27e77
devanshV18 <devanshverma024@gmail.com>
2023-09-17 19:21:09 +0530

    Nav started

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,40 @@
 import {Hero, CustomerReviews,SuperQuality, sub, SplOffer,PopularProducts,Footer, Services } from './sections';
+import Nav from './Components/Nav';
+
 
 const App = () => (
   <main className = 'relative'>
     <Nav/>
     <section className="xl:padding-1 wide:padding-r padding-b">
-      <Hero/>
+      <Hero/> 
     </section>
 
     <section className = "padding">
-      <PopularProducts/>
+      <PopularProducts/> PopularProducts
     </section>
 
     <section className = "padding">
-      <SuperQuality/>
+      <SuperQuality/> SuperQuality
     </section>
 
     <section className = "padding-x py-10">
-      <Services/>
+      <Services/> Services
     </section>
 
     <section className = "padding">
-      <SplOffer/>
+      <SplOffer/> SplOffer
     </section>
 
     <section className = "padding bg-pale-blue">
-      <CustomerReviews/>
+      <CustomerReviews/> CustomerReviews
     </section>
 
     <section className = "padding-x sm:py-32 py-16 w-full">
-     <sub/>
+     <sub/> sub
     </section>
 
     <section className = "bg-black padding-x padding-t pb-8">
-      <Footer/>
+      <Footer/> Footer
     </section>
   </main>
 );
