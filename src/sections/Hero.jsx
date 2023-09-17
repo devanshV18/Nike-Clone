95c26ac983cadb18cc0de0e8fc5922916c3f0b63
devanshV18 <devanshverma024@gmail.com>
2023-09-17 20:24:34 +0530

    Button problem

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,8 +1,6 @@
-import Button from '../Components/Button';
-import { arrowRight } from '../assets/icons';
 const Hero = () => {
   return (
-   <section id="home" className="w-full flex xl:flex-row flex-col justify-center min-h-screengap-10 max-container">
+   <section id="home" className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container border-2 border-red-600">
       <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
           <p>Our Summer Collection</p>
             <h1>
@@ -12,8 +10,8 @@ const Hero = () => {
               Shoes
             </h1>
             <p>Discover Stylish Nike arrivals, quality comfort, and innovation for your active life</p>
-            {/* <Button label = "Shop Now"  
-            iconURL={arrowRight}/> */}
+            <Button/>
+            
       </div>
    </section>
   )
